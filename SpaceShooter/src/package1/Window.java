8734204ee5d29004ec85cc324300d18e293a30cd
lukysswek <lksreich@gmail.com>
2020-01-04 19:04:02 +0100

    A tractor is an engineering vehicle specifically designed to deliver a high tractive effort (or torque) at slow speeds, for the purposes of hauling a trailer or machinery used in agriculture or construction. Most commonly, the term is used to describe a farm vehicle that provides the power and traction to mechanize agricultural tasks, especially (and originally) tillage, but nowadays a great variety of tasks. Agricultural implements may be towed behind or mounted on the tractor, and the tractor may also provide a source of power if the implement is mechanised.

--- SpaceShooter/src/package1/Window.java
+++ SpaceShooter/src/package1/Window.java
@@ -1,8 +1,14 @@
 package package1;
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Window extends JFrame {
+	private JButton exit;
 	public Window() {
 		super("EPIC TITLE");
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -11,6 +17,22 @@ public class Window extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+		setLayout(null);
+		
+		
+		JButton exit = new JButton("EXIT");
+		exit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				
+			}
+		});
+		exit.setBounds(getWidth()/2-100,getHeight()/2-75,100,75);
+		add(exit);
+		
+		
 	}
 	public static void main(String[] args) {
 		new Window();
