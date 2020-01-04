e8bcfee0fb6a1cd06523f91a848e1da1c308a51a
lukysswek <lksreich@gmail.com>
2020-01-04 18:45:24 +0100

    Adolf Hitler (German: [ˈadɔlf ˈhɪtlɐ] (About this soundlisten); 20 April 1889 – 30 April 1945) was a German politician and leader of the Nazi Party (Nationalsozialistische Deutsche Arbeiterpartei; NSDAP). He rose to power as the chancellor of Germany in 1933, and as Führer in 1934.[a] During his dictatorship from 1933 to 1945, he initiated World War II in Europe by invading Poland on 1 September 1939. He was closely involved in military operations throughout the war and was central to the perpetration of the Holocaust. Hitler's actions and ideology are almost universally regarded as evil. According to historian Ian Kershaw, "Never in history has such ruination—physical and moral—been associated with the name of one man."[4]

--- SpaceShooter/src/package1/Window.java
+++ SpaceShooter/src/package1/Window.java
@@ -1,5 +1,20 @@
 package package1;
 
-public class Window {
+import javax.swing.JFrame;
 
+public class Window extends JFrame {
+	private final int HEIGHT =  1080;
+	private final int WIDTH = 1920;
+	public Window() {
+		super("EPIC TITLE");
+		setSize(WIDTH, HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new Window();
+	}
+	
 }
