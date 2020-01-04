281db9629e2de3562e8389dbff947e947e0edb5b
Ondrej Vainer <vainerondrej@gmail.com>
2020-01-04 18:47:21 +0100

    comit

---
+++ SpaceShooter/src/package1/Meteor.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Meteor extends GameObject {
+	public Meteor(int[][] corners, int[] rotationPoint, int rotationAngle) {
+		super(corners, rotationPoint, rotationAngle);
+		
+	}
+
+}
