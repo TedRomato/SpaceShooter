df619ba387a0a193d763140c5ecda5e09440e677
lukysswek <lksreich@gmail.com>
2020-01-08 19:51:36 +0100

    Karel

--- SpaceShooter/src/package1/Window.java
+++ SpaceShooter/src/package1/Window.java
@@ -1,20 +1,29 @@
 package package1;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Window extends JFrame {
 	private JButton exit, start;
-	private boolean running = false;
+	private boolean running = true;
+	private Graphics g;
+	private BufferStrategy bs;
+	private Corner c1,c2,c3;
+	private Corner[] corners = new Corner[3];
+	private GameObject GO;
 	public Window() {
 		super("EPIC TITLE");
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setResizable(false);
-		setUndecorated(true);
+		setSize(1280,720);
+		//setUndecorated(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
@@ -25,7 +34,7 @@ public class Window extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				running = true;
+				start();
 			}
 		});
 		start.setBounds(getWidth()/2-150,getHeight()/2-75, 150, 75);
@@ -43,11 +52,17 @@ public class Window extends JFrame {
 		exit.setBounds(getWidth()/2-150,getHeight()/2,150,75);
 		add(exit);
 		
+		c1 = new Corner(50,50);
+		corners[0] = c1;
+		c2 = new Corner(75,50);
+		corners[1] = c2;
+		c3 = new Corner(50,75);
+		corners[2] = c3;
 		
 	}
 	public void start() {
 		long lastTime = System.nanoTime();
-        double amountOfTicks = 120.0;
+        double amountOfTicks = 60.0;
         double ns = 1000000000 / amountOfTicks;
         double delta = 0;
         long timer = System.currentTimeMillis();
@@ -82,6 +97,18 @@ public class Window extends JFrame {
 		}
 	}
 	public void render() {
+		GO = new GameObject(corners);
+		bs = getBufferStrategy();
+        if(bs == null) {
+         createBufferStrategy(3);
+         return;
+        }   
+        g = bs.getDrawGraphics();
+        GO.render(g);
+        g.clearRect(0,0,WIDTH,HEIGHT);
+		g.setColor(Color.BLUE);
+		bs.show();
+		g.dispose();
 		
 			
 	}
