a2ad57981a915e0d2e45f008572eccd34cbc78f1
lukysswek <lksreich@gmail.com>
2020-01-08 19:14:30 +0100

    Loop + GameObject.render

--- SpaceShooter/src/package1/Corner.java
+++ SpaceShooter/src/package1/Corner.java
@@ -124,4 +124,21 @@ public class Corner {
 		return (int) Math.round(Math.sqrt(x*x + y*y));
 		
 	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 }
