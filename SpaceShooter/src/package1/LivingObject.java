f8f934786cd8a2fdefa19b9525787d3070abeae0
lukysswek <lksreich@gmail.com>
2020-05-04 20:14:45 +0200

    ads

--- SpaceShooter/src/package1/LivingObject.java
+++ SpaceShooter/src/package1/LivingObject.java
@@ -365,14 +365,7 @@ public class LivingObject extends MovingObject{
 		return maxSpeed;
 	}
 	
-	private static BufferedImage resize(BufferedImage img, int width, int height) {
-        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = resized.createGraphics();
-        g2d.drawImage(tmp, 0, 0, null);
-        g2d.dispose();
-        return resized; 
-    }
+
 	
 	public void render(Graphics g) {
 		super.render(g);
